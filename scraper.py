a4212ac300087889ef19c1b9bc2ebbbd48428f2f
Daniel Williams <daniel.mays.williams@gmail.com>
2023-11-08 18:21:24 -0500

    Updated webdriver-manager syntax

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.common.by import By
 from transformers import pipeline
@@ -12,7 +12,7 @@ import datetime
 
 start = time.time()
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chrome_service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
